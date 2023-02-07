40255ba183a903a1bfada1bd0f7d5f4255491031
Sbat2000 <103517615+Sbat2000@users.noreply.github.com>
2023-02-07 22:53:19 +0800

    add rules VC

---
+++ Millionaire/Controllers/RulesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RulesViewController.swift
+//  Millionaire
+//
+//  Created by Александр Гарипов on 07.02.2023.
+//
+
+import UIKit
+
+class RulesViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
